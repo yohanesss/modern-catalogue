7d073a5182c9286083c3120969e87fe6c6b79856
Yohanes Setiawan <test27c@gmail.com>
2022-11-01 13:43:01 -0700

    initial commit

---
+++ src/routes/navigation/navigation.component.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import { Link, Outlet } from "react-router-dom";
+
+const Navigation = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  return (
+    <>
+      <header>
+        <div className="logo-container">
+          <div>
+            <span>Modern</span>
+            <span>Catalogue</span>
+          </div>
+        </div>
+        <div className="category-dropdown">Category!</div>
+        <div className="search-container"></div>
+        <div className="navigation-container">
+          <ul>
+            <li>
+              <Link to="/shop">Shop</Link>
+            </li>
+            <li>
+              <Link to="/about-us">About Us</Link>
+            </li>
+            <li>
+              <Link to="/contact-us">Contact Us</Link>
+            </li>
+            <li>
+              {loggedIn ? (
+                <span>User</span>
+              ) : (
+                <Link to="authentication">Log In</Link>
+              )}
+            </li>
+          </ul>
+          <div className="shopping-cart-icon">
+            <h3>Cart</h3>
+          </div>
+        </div>
+      </header>
+      <Outlet />
+    </>
+  );
+};
+
+export default Navigation;
